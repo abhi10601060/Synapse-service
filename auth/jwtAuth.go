1930e2cb82f49a5d7d0b2720ff48f7e32accce93
Abhi <abhishekvelekar79200@gmail.com>
2024-06-16 19:13:58 +0530

    jwt Token authorization middleware added

---
+++ auth/jwtAuth.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"log"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	secret_key = []byte("Synapse_Rocks")
+)
+
+type claims struct {
+	UserName string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func IsAuthorizedToken(tokenStr string) (bool, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &claims{}, func(t *jwt.Token) (interface{}, error) { return secret_key, nil })
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			log.Println("signature is invalid...")
+			return false, nil
+		}
+		log.Println("Bad Request...")
+		return false, err
+	}
+
+	if !token.Valid {
+		log.Println("Unauthorized token...")
+		return false, nil
+	}
+
+	return true, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"synapse/stream/auth"
+	"synapse/stream/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Welcome to Synapse Stream")
+
+	r := gin.Default()
+
+	r.GET("/start", authorize, handler.StartStream)
+
+	r.Run(":8010")
+}
+
+func authorize(c *gin.Context) {
+	tokenStr := c.Request.Header.Get("Authentication-Token")
+
+	if tokenStr == "" {
+		c.JSON(405, gin.H{
+			"message": "Header token missing",
+		})
+		c.Abort()
+		return
+	}
+
+	isTokenValid, err := auth.IsAuthorizedToken(tokenStr)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"message": "In correct Header Token",
+		})
+		c.Abort()
+		return
+	}
+
+	if !isTokenValid {
+		c.JSON(401, gin.H{
+			"message": "Unauthorized Header Token",
+		})
+		c.Abort()
+		return
+	}
+	log.Println("Authorization Passed wit valid token...")
 }
